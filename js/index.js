993649be2fb16cd85609a44d384cfa091abd686e
AlexCheka <alex.cheka87@gmail.com>
2020-09-26 16:30:36 +0000

    fin

---
+++ js/index.js
@@ -0,0 +1,50 @@
+document.querySelector('.time').innerText
+  = new Date().toLocaleTimeString();
+
+document.querySelector('.get-ajax-html').addEventListener('click', getAjaxHTML);
+
+function getAjaxHTML() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector('.html-container') 
+                .innerHTML = xhr.responseText;   
+        }
+    }
+    xhr.open('get', 'client-data.html', true);
+    xhr.send();
+}
+
+document.querySelector('.fetch-html').addEventListener('click', fetchHTML);
+
+async function fetchHTML() {
+    // fetch('client-data.html')
+    // .then( response => response.text() )
+    // .then( html => document.querySelector('.html-container').innerHTML = html );
+    const response = await fetch('client-data.html');
+    document.querySelector('.html-container').innerHTML = await response.text();
+}
+
+document.querySelector('.get-ajax-json').addEventListener('click', getAjaxJson);
+
+function getAjaxJson() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            document.querySelector('.client-name').innerText = data.name;  
+            document.querySelector('.account-balance').innerText = data.balance;  
+        }
+    }
+    xhr.open('get', 'client-data.json', true);
+    xhr.send();
+}
+
+document.querySelector('.fetch-json').addEventListener('click', fetchJson);
+
+async function fetchJson() {
+    const response = await fetch('client-data.json');
+    const data = await response.json();
+    document.querySelector('.client-name').innerText = data.name;  
+    document.querySelector('.account-balance').innerText = data.balance;  
+}
